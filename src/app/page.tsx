8bd85095d4963a62926533608a360a4d69d67024
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-08-01 10:06:55 +0500

    text and image clipping effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className="min-h-[60vh] flex items-center justify-center">
+      <div className="bg-gradient-to-tr from-cyan-200 to-pink-200 rounded-3xl">
+        <h1 className="font-bold text-8xl uppercase bg-[size:130%] bg-[url('/image.jpeg')] bg-clip-text text-transparent animate-slow-pan border p-3">
+          Trello Clone
+        </h1>
+      </div>
+    </div>
+  );
 }
